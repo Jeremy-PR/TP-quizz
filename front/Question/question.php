13884c100ae03329fce045f679b551b0b0a5c125
aliii390 <allli.said.errahmani@gmail.com>
2024-12-12 17:07:13 +0100

    ADD: structure final fin des modifications

--- front/Question/question.php
+++ front/Question/question.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question</title>
-    <link rel="stylesheet" href="./question.css">
+    <link rel="stylesheet" href="../../css/question.css">
 </head>
 <body>
 
