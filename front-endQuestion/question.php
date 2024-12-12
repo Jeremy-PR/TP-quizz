efd7f63a855235d505edede3026716495a6ab059
aliii390 <allli.said.errahmani@gmail.com>
2024-12-12 16:26:21 +0100

    ADD: fin de la partie question

--- front-endQuestion/question.php
+++ front-endQuestion/question.php
@@ -4,8 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question</title>
+    <link rel="stylesheet" href="./question.css">
 </head>
 <body>
+
+<main>
+
+<section class="question">
+    <h2> < QUESTION-<span style="color: #9B5EBF;"> 1/> </span> </h2>
     
+    
+    <form action="" method="">
+    <div class="btn-placement">
+        <div class="h3-placement">
+            <h3>Ici on pose la question ?</h3>
+        </div>
+
+        <button>Super héros</button>
+        <button>Super héros</button>
+        <button class="hover-green">Validez</button>
+    </div>
+    </form>
+   
+</section>
+
+<section class="question">
+    <h2> < QUESTION-<span style="color: #9B5EBF;"> 2/> </span> </h2>
+    
+    <form action="" method="">
+    <div class="btn-placement">
+        <div class="h3-placement">
+            <h3>Ici on pose la question ?</h3>
+        </div>
+
+        <button>Super héros</button>
+        <button>Super héros</button>
+        <button class="hover-green">Validez</button>
+    </div>
+    </form>
+</section>
+
+<section class="question">
+    <h2> < QUESTION-<span style="color: #9B5EBF;"> 3/> </span> </h2>
+    
+    <form action="" method="">
+    <div class="btn-placement">
+        <div class="h3-placement">
+            <h3>Ici on pose la question ?</h3>
+        </div>
+
+        <button>Super héros</button>
+        <button>Super héros</button>
+        <button class="hover-green">Validez</button>
+    </div>
+    </form>
+</section>
+
+
+<input type="submit" class="btn-submit" value="Voir les résultats">
+
+</main>
+
 </body>
 </html>
