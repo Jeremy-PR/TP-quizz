8da6a947425d6afb4af1b5283533e40a1a653370
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-12-13 11:44:58 +0100

    add : test

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <h1> < QUIZZ<span style="color: #9B5EBF;"> 404/> </span> </h1>
     <div id="accueil">
         <h2>Connectez vous à votre compte</h2>
+        
         <input type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme">
         <a href="./front/Acceuil/accueil.php">Jouer</a>
     </div>
