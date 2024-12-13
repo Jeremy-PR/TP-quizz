3fe2289b496edd773b4129e7667895c8ef7e7df1
aliii390 <allli.said.errahmani@gmail.com>
2024-12-13 09:53:39 +0100

    ADD: fin de la partie résultat

--- front/Resultat/resultat.php
+++ front/Resultat/resultat.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Résultat</title>
+    <link rel="stylesheet" href="../../css/resultat.css">
 </head>
 <body>
-    
+
+
+
+
+<section class="resultat">
+    <h1>Bien jouer vous avez 3/3</h1>
+
+    <a href="">Retournez au menu d'accueil</a>
+</section>
+
+
+
+
+
 </body>
 </html>
